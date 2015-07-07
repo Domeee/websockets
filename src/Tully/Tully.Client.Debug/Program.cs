be04a98a8c29b0ce87b1afcb3512802c9b87e6cd
Domeee <idshedsdi@gmail.com>
2015-07-07 19:04:02 +0200

    Initial client code

--- src/Tully/Tully.Client.Debug/Program.cs
+++ src/Tully/Tully.Client.Debug/Program.cs
@@ -34,6 +34,33 @@ namespace Tully.Client.Debug
                 var stream = client.GetStream();
                 stream.Write(data, 0, data.Length);
                 Console.WriteLine("Sent: {0}", message);
+
+                // Client single message
+                byte byte1 = 129;
+                byte byte2 = 131;
+                var maskingKey = new byte[4];
+                var rnd = new Random();
+                rnd.NextBytes(maskingKey);
+                var decoded = Encoding.UTF8.GetBytes("MDN");
+                var encoded = new byte[decoded.Length];
+
+                for (int i = 0; i < encoded.Length; i++)
+                {
+                    encoded[i] = (byte)(decoded[i] ^ maskingKey[i % 4]);
+                }
+
+                var frame = new byte[9];
+                frame[0] = byte1;
+                frame[1] = byte2;
+                frame[2] = maskingKey[0];
+                frame[3] = maskingKey[1];
+                frame[4] = maskingKey[2];
+                frame[5] = maskingKey[3];
+                frame[6] = encoded[0];
+                frame[7] = encoded[1];
+                frame[8] = encoded[2];
+
+                stream.Write(frame, 0, frame.Length);
                 
                 stream.Close();
                 client.Close();
